8f12b2c88f861d07ce4e482591490c63a3964c92
Hoan <balaminmau@gmail.com>
2026-09-03 17:53:36 +0700

    fix: change maintain_max EXP from 8 to 7

--- nihongo-master/src/lib/srs/pointsEngine.ts
+++ nihongo-master/src/lib/srs/pointsEngine.ts
@@ -203,7 +203,7 @@ export async function recordSrsExp(
     let awardedExp = 0;
     if (type === 'new') awardedExp = 5;
     else if (type === 'review_up') awardedExp = level || 1;
-    else if (type === 'maintain_max') awardedExp = 8;
+    else if (type === 'maintain_max') awardedExp = 7;
     else awardedExp = 1;
 
     const updates: any = {
